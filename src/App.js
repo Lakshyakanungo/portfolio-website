a49ff52a2929485770556554b1750d44cd1aabb7
lakshya_kanungo <lakshya.ecowrap@gmail.com>
2023-02-05 16:54:09 +0530

    home section started

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import ContactBar from "./Components/ContactBar";
 import "./App.css";
 
 function App() {
-  return <p>Hello</p>;
+  return (
+    <main className="main">
+      <Navbar />
+      <div className="app_section_container">
+        <ContactBar />
+      </div>
+      <Footer />
+    </main>
+  );
 }
 
 export default App;
