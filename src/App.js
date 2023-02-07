a2529dfe4f1d785175420ba7dcce8e9f9f927b0d
lakshya_kanungo <lakshya.ecowrap@gmail.com>
2023-02-07 15:56:49 +0530

    added images resource

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import ContactBar from "./Components/ContactBar";
 import "./App.css";
+import Home from "./Sections/Home";
 
 function App() {
   return (
     <main className="main">
       <Navbar />
       <div className="app_section_container">
+        <Home />
         <ContactBar />
       </div>
       <Footer />
